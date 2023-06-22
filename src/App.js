e4c0464c66bf4dca102d1e15823c40715e206f66
Anzori-PR <saxelashvilianzori@gmail.com>
2023-06-22 21:34:10 +0400

    continiou

--- src/App.js
+++ src/App.js
@@ -6,39 +6,17 @@ import Search from './WorkSpace/search/search';
 import Hero from './WorkSpace/hero/hero';
 import Hero2 from './WorkSpace/hero/hero2';
 import Footer from './WorkSpace/footer/footer';
-import Registration from './WorkSpace/registration/registration';
-import Signin from './WorkSpace/registration/Signin';
 
 function App() {
-  const [isRegistrationOpen, setRegistrationOpen] = useState(false);
-  const [isSigninOpen, setSigninOpen] = useState(false);
-
-  const openRegistration = () => {
-    setRegistrationOpen(true);
-  };
-
-  const closeRegistration = () => {
-    setRegistrationOpen(false);
-  };
-
-  const openSignin = () => {
-    setSigninOpen(true);
-  };
-
-  const closeSignin = () => {
-    setSigninOpen(false);
-  };
-
   return (
     <div>
-      <Header openRegistration={openRegistration} openSignin={openSignin} />
+      <Header/>
       <Menu />
       <Search />
       <Hero />
       <Hero2 />
       <Footer />
-      {isRegistrationOpen && <Registration onClose={closeRegistration} openSignin={openSignin}/>}
-      {isSigninOpen && <Signin onClose={closeSignin} openRegistration={openRegistration}/>}
+
     </div>
   );
 }
--- src/WorkSpace/header/header.jsx
+++ src/WorkSpace/header/header.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function header({ openRegistration, openSignin}) {
+function Header() {
+
     return (
         <div className='p-1'>
             <header className='container flex flex-row justify-between py-4'>
-                <div class='logo flex flex-row items-center'>
-                    <h1 className='text-black font-bold lg:text-3xl md:text-3xl text-2xl'> Bucket </h1>
+                <div className='logo flex flex-row items-center'>
+                    <h1 className='text-black font-bold lg:text-3xl md:text-3xl text-2xl'>Bucket</h1>
                 </div>
-                <div className='register flex row items-center'>
-                    <div className='cursor-pointer no-underline button border-black border-2 bg-white text-black rounded-3xl p-1 px-2 text-xs md:text-sm lg:text-sm mx-3' onClick={openRegistration}>რეგისტრაცია</div>
-                    <div className='cursor-pointer no-underline button border-black border-2 bg-black text-white rounded-3xl p-1 px-4 text-xs md:text-sm lg:text-sm' onClick={openSignin}>შესვლა</div>
+                <div>
+                    
                 </div>
             </header>
-
             <div className='line border-b-2 border-gray-100'></div>
         </div>
     );
 }
 
-export default header;
+export default Header;
